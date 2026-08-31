cafe14611995892d23ee1fba8e86511803dc86f1
presori <presori@boom.al>
2026-08-31 20:18:05 +0200

    User Session view config update

--- modules/core/database/schemas/userSession/userSession.views.ts
+++ modules/core/database/schemas/userSession/userSession.views.ts
@@ -21,7 +21,6 @@ export const userSessionSheetView: ViewConfig = {
                     children: [
                         {
                             render: "#DisplayCard",
-                            dependent: "user",
                             permissions: {read: "user"},
                             field: {
                                 name: "user",
